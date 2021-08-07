1a9814c791dc9995a59ae29af06c7d161bc116c6
KeiZ0r <keizisc2@gmail.com>
2021-08-07 14:49:49 +0300

    init

---
+++ Ei_Dimension/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Ei_Dimension
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+  }
+}
---
+++ Ei_Dimension/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿using DevExpress.Mvvm;
+using System;
+
+namespace Ei_Dimension.ViewModels
+{
+  public class MainViewModel : ViewModelBase
+  {
+  }
+}
