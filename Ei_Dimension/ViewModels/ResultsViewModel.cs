84bd3b8b267cad1b532ca919b921ad108255040d
KeiZ0r <keizisc2@gmail.com>
2021-08-08 14:27:12 +0300

    ui be4 mainwin

---
+++ Ei_Dimension/ViewModels/ResultsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using System;
+using DevExpress.Mvvm.POCO;
+namespace Ei_Dimension.ViewModels
+{
+  [POCOViewModel]
+  public class ResultsViewModel
+  {
+    protected ResultsViewModel()
+    {
+        
+    }
+
+    public static ResultsViewModel Create()
+    {
+      return ViewModelSource.Create(() => new ResultsViewModel());
+    }
+  }
+}
