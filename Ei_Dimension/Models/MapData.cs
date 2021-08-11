adfe6690e6eaf512c7e73b356100ae308b493ba9
KeiZ0r <keizisc2@gmail.com>
2021-08-11 20:54:05 +0300

    map working

---
+++ Ei_Dimension/Models/MapData.cs
@@ -0,0 +1,15 @@
+﻿using Ei_Dimension.Core;
+
+namespace Ei_Dimension.Models
+{
+  public class MapData : ObservableObject
+  {
+    public int X { get; set; }
+    public int Y { get; set; }
+    public MapData(int x, int y)
+    {
+      X = x;
+      Y = y;
+    }
+  }
+}
--- Ei_Dimension/ViewModels/ResultsViewModel.cs
+++ Ei_Dimension/ViewModels/ResultsViewModel.cs
@@ -20,18 +20,28 @@ namespace Ei_Dimension.ViewModels
     public virtual ObservableCollection<HistogramData> RedSsc { get; set; }
     public virtual ObservableCollection<HistogramData> GreenSsc { get; set; }
     public virtual ObservableCollection<HistogramData> Reporter { get; set; }
+    public virtual ObservableCollection<int> SeriesCL0 { get; set; }
+    public virtual ObservableCollection<int> SeriesCL1 { get; set; }
+    public virtual ObservableCollection<int> SeriesCL2 { get; set; }
+    public virtual ObservableCollection<int> SeriesCL3 { get; set; }
     public virtual ObservableCollection<ResultFile> AvailableResults { get; set; }
     public virtual ObservableCollection<ResultFile> SelectedItem { get; set; }
+    public virtual ObservableCollection<MapData> HeatMapSeriesXY { get; set; }
+    public virtual string HeatmapAxisXTitle { get; set; }
+    public virtual string HeatmapAxisYTitle { get; set; }
 
     private List<BeadInfoStruct> _beadStructsList;
     private List<SortedDictionary<int, int>> _histoDicts;
+    private (int, int) _heatmapAxisXY;
 
     protected ResultsViewModel()
     {
       GetAvailableResults();
       SelectedItem = new ObservableCollection<ResultFile>();
-      
-
+      HeatMapSeriesXY = new ObservableCollection<MapData>();
+      HeatmapAxisXTitle = "CL1";
+      HeatmapAxisYTitle = "CL2";
+      _heatmapAxisXY = (1,2);
     }
 
     public static ResultsViewModel Create()
@@ -72,7 +82,7 @@ namespace Ei_Dimension.ViewModels
       }
     }
     [AsyncCommand(UseCommandManager = false)]
-    public async void FillReporterAsync()
+    public async void FillAllDataAsync()
     {
       if (_histoDicts != null)
       {
@@ -94,6 +104,9 @@ namespace Ei_Dimension.ViewModels
       GreenTask.Start();
       Task<ObservableCollection<HistogramData>> ReporterTask = new Task<ObservableCollection<HistogramData>>(AddReporterItem);
       ReporterTask.Start();
+      Task<(ObservableCollection<int>, ObservableCollection<int>, ObservableCollection<int>, ObservableCollection<int>)> MapTask =
+        new Task<(ObservableCollection<int>, ObservableCollection<int>, ObservableCollection<int>, ObservableCollection<int>)>(AddMapItem);
+      MapTask.Start();
       await ForwardTask;
       ForwardSsc = ForwardTask.Result;
       await VioletTask;
@@ -104,6 +117,98 @@ namespace Ei_Dimension.ViewModels
       GreenSsc = GreenTask.Result;
       await ReporterTask;
       Reporter = ReporterTask.Result;
+      await MapTask;
+      (SeriesCL0, SeriesCL1, SeriesCL2, SeriesCL3) = MapTask.Result;
+      UpdateHeatmap();
+    }
+
+    public void ChangeHeatmapX(int index)
+    {
+      switch (index)
+      {
+        case 0:
+          _heatmapAxisXY.Item1 = 0;
+          HeatmapAxisXTitle = "CL0";
+          break;
+        case 1:
+          _heatmapAxisXY.Item1 = 1;
+          HeatmapAxisXTitle = "CL1";
+          break;
+        case 2:
+          _heatmapAxisXY.Item1 = 2;
+          HeatmapAxisXTitle = "CL2";
+          break;
+        case 3:
+          _heatmapAxisXY.Item1 = 3;
+          HeatmapAxisXTitle = "CL3";
+          break;
+      }
+      UpdateHeatmap();
+    }
+    public void ChangeHeatmapY(int index)
+    {
+      switch (index)
+      {
+        case 0:
+          _heatmapAxisXY.Item2 = 0;
+          HeatmapAxisYTitle = "CL0";
+          break;
+        case 1:
+          _heatmapAxisXY.Item2 = 1;
+          HeatmapAxisYTitle = "CL1";
+          break;
+        case 2:
+          _heatmapAxisXY.Item2 = 2;
+          HeatmapAxisYTitle = "CL2";
+          break;
+        case 3:
+          _heatmapAxisXY.Item2 = 3;
+          HeatmapAxisYTitle = "CL3";
+          break;
+      }
+      UpdateHeatmap();
+    }
+    public void UpdateHeatmap()
+    {
+      HeatMapSeriesXY.Clear();
+      ObservableCollection<int> x = new ObservableCollection<int>();
+      ObservableCollection<int> y = new ObservableCollection<int>();
+      switch (_heatmapAxisXY.Item1)
+      {
+        case 0:
+          x = SeriesCL0;
+          break;
+        case 1:
+          x = SeriesCL1;
+          break;
+        case 2:
+          x = SeriesCL2;
+          break;
+        case 3:
+          x = SeriesCL3;
+          break;
+      }
+      switch (_heatmapAxisXY.Item2)
+      {
+        case 0:
+          y = SeriesCL0;
+          break;
+        case 1:
+          y = SeriesCL1;
+          break;
+        case 2:
+          y = SeriesCL2;
+          break;
+        case 3:
+          y = SeriesCL3;
+          break;
+      }
+      int i = 0;
+      while (i < x.Count)
+      {
+        HeatMapSeriesXY.Add(new MapData(x[i],y[i]));
+        i++;
+      }
     }
 
     private string GetDataFromFile(string path)
@@ -195,7 +300,7 @@ namespace Ei_Dimension.ViewModels
         {
           dictReporter.Add(key, 1);
         }
-
+        
         key = (int)bs.fsc;
         if (dictForward.ContainsKey(key))
         {
@@ -290,6 +395,22 @@ namespace Ei_Dimension.ViewModels
         col.Add(new HistogramData(x.Value, x.Key));
       }
       return col;
+    }
+    //AddMapItem is different
+    private (ObservableCollection<int>, ObservableCollection<int>, ObservableCollection<int>, ObservableCollection<int>) AddMapItem()
+    {
+      ObservableCollection<int> CL0 = new ObservableCollection<int>();
+      ObservableCollection<int> CL1 = new ObservableCollection<int>();
+      ObservableCollection<int> CL2 = new ObservableCollection<int>();
+      ObservableCollection<int> CL3 = new ObservableCollection<int>();
+      foreach (var bs in _beadStructsList)
+      {
+        CL0.Add((int)bs.cl0);
+        CL1.Add((int)bs.cl1);
+        CL2.Add((int)bs.cl2);
+        CL3.Add((int)bs.cl3);
+      }
+      return (CL0, CL1, CL2, CL3);
     }
     #endregion
   }
