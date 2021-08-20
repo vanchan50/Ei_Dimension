a6ae97f8590f0e66411a76e802ebf82ca711c953
KeiZ0r <keizisc2@gmail.com>
2021-08-21 02:26:31 +0300

    Styles Revamp. Maintenance ready. Bugfixes

--- Ei_Dimension/ViewModels/ChannelsViewModel.cs
+++ Ei_Dimension/ViewModels/ChannelsViewModel.cs
@@ -7,8 +7,36 @@ namespace Ei_Dimension.ViewModels
   [POCOViewModel]
   public class ChannelsViewModel
   {
+    public virtual string[] Bias30Parameters { get; set; }
+    public virtual string[] TcompBiasParameters { get; set; }
+    public virtual string[] TempParameters { get; set; }
+    public virtual string[] BackgroundParameters { get; set; }
+    public virtual string SiPMTempCoeff { get; set; }
+
     protected ChannelsViewModel()
     {
+      Bias30Parameters = new string[10];
+      Bias30Parameters[0] = "1180";
+      Bias30Parameters[1] = "1500";
+      Bias30Parameters[2] = "1500";
+      Bias30Parameters[4] = "1330";
+      Bias30Parameters[5] = "1180";
+      Bias30Parameters[6] = "1180";
+      Bias30Parameters[7] = "1900";
+      TcompBiasParameters = new string[10];
+      TempParameters = new string[10];
+      BackgroundParameters = new string[10];
+
+    }
+
+    public void UpdateBiasButtonClick()
+    {
+
+    }
+
+    public void SaveBiasButtonClick()
+    {
+
     }
 
     public static ChannelsViewModel Create()
--- Ei_Dimension/ViewModels/MaintenanceViewModel.cs
+++ Ei_Dimension/ViewModels/MaintenanceViewModel.cs
@@ -8,16 +8,37 @@ namespace Ei_Dimension.ViewModels
   [POCOViewModel]
   public class MaintenanceViewModel
   {
+    public virtual bool LEDsToggleButtonState { get; set; }
+    public virtual object LEDSliderValue { get; set; }
+    public virtual string SanitizeSecondsContent { get; set; }
+
+
     private INavigationService NavigationService { get { return this.GetService<INavigationService>(); } }
 
     protected MaintenanceViewModel()
     {
-        
+      LEDSliderValue = 0;
+      LEDsToggleButtonState = false;
     }
 
     public static MaintenanceViewModel Create()
     {
       return ViewModelSource.Create(() => new MaintenanceViewModel());
+    }
+
+    public void LEDsButtonClick()
+    {
+      LEDsToggleButtonState = !LEDsToggleButtonState;
+    }
+
+    public void LEDSliderValueChanged()
+    {
+      SanitizeSecondsContent = LEDSliderValue.ToString();
+    }
+
+    public void UVCSanitizeClick()
+    {
+
     }
 
     public void NavigateCalibration()
